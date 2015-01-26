fb721db579c7b551d93fae93b6af9a30ae11e4d5
Christina Moulton <christina@teakmobile.com>
2015-01-26 08:51:09 -0400

    First pass, just a straight port from Obj-C

--- UiKeyboardDismiss/ViewController.swift
+++ UiKeyboardDismiss/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  var keyboardDismissTapGesture: UIGestureRecognizer?
+  var textfield: UITextField?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    textfield = UITextField(frame: CGRect(x: 20, y: 20, width: self.view.frame.size.width - 40, height: 40))
+    textfield!.borderStyle = UITextBorderStyle.Line
+    self.view.addSubview(textfield!)
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+    NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
+
   }
-
-
+  
+  func keyboardWillShow(notification: NSNotification) {
+    if keyboardDismissTapGesture == nil
+    {
+      keyboardDismissTapGesture = UITapGestureRecognizer(target: self, action: Selector("dismissKeyboard:"))
+      self.view.addGestureRecognizer(keyboardDismissTapGesture!)
+    }
+  }
+  
+  func keyboardWillHide(notification: NSNotification) {
+    if keyboardDismissTapGesture != nil
+    {
+      self.view.removeGestureRecognizer(keyboardDismissTapGesture!)
+      keyboardDismissTapGesture = nil
+    }
+  }
+  
+  func dismissKeyboard(sender: AnyObject) {
+    textfield?.resignFirstResponder()
+  }
 }
 
